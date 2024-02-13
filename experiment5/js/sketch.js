57ab60f1b444663fb5bbe6ff24eb1b35d3a0423e
ajhalim <thenubbum@gmail.com>
2024-02-12 23:32:50 -0800

    sad

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -1,17 +1,23 @@
-function setup() {
+  function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
-       normalMaterial();
-  
+     normalMaterial();
+    describe(
+      'Camera orbits around a box when mouse is hold-clicked & then moved.'
+    );
   }
   
-  let angle = 0
-  let size = 0;
+  let thing = 0
+  
   function draw() {
     background(200);
     orbitControl();
     rotateX(mouseX * 0.01);
     rotateY(mouseY * 0.01);
-      //angle += 0.01
-    size = (mouseX + mouseY) * 0.2;
-    cone(size);
+    thing = (mouseX + mouseY) * 0.25
+    if(thing > 200){
+      box(thing);
+    }else{
+      sphere(thing);
+    }
+    color(thing);
   }
