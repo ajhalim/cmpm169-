9f2f72bb22260fb8de3933d2296feb63f77f76e4
ajhalim <thenubbum@gmail.com>
2024-02-21 02:09:03 -0800

    Update sketch.js

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -1,67 +1,25 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+var font = 'sans-serif';
+var letter = ':3 :3 :3 :3 :3 :3';
+var letter2 = ':3 :3 :3 :3 :3 :3';
+var angle = 0;
+var size = 0;
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
+    createCanvas(windowWidth, windowHeight);
+    background(255);
+    fill(0);
+  
+    textFont(font);
+    textAlign(CENTER, CENTER);
+  }
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
-}
-
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+    clear();
+    textSize((mouseX - width / 2) * 5 + 1);
+    text(letter, width / 2, mouseY);
+    rotate(angle);
+    angle++;
+    fill(mouseX/2, mouseY/2, angle/2);
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
-}
